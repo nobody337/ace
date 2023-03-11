1f6f74c2086da53e250ae57ae354fdc8b64c29ba
nobody337 <127604866+nobody337@users.noreply.github.com>
2023-03-11 14:13:38 +0100

    Create bruteforce.py

---
+++ bruteforce.py
@@ -0,0 +1,9 @@
+import time
+
+def BruteForce():
+    print("Enter the social media or email address you want to crack the password for: ")
+    target = input()
+    print(f"Cracking {target} password...")
+    time.sleep(5)
+    print("Password cracked!")
+    print("Password: iloveyou")
