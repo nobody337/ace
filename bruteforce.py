0e6a599f70ff9de05795ad9c3d7b27d50c42e874
nobody337 <127604866+nobody337@users.noreply.github.com>
2023-03-11 16:22:38 +0100

    Delete bruteforce.py

--- bruteforce.py
+++
@@ -1,9 +0,0 @@
-import time
-
-def BruteForce():
-    print("Enter the social media or email address you want to crack the password for: ")
-    target = input()
-    print(f"Cracking {target} password...")
-    time.sleep(5)
-    print("Password cracked!")
-    print("Password: iloveyou")
