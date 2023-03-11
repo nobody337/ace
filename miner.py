af9622a620574fdb059e825ad731108a881be61a
nobody337 <127604866+nobody337@users.noreply.github.com>
2023-03-11 16:37:36 +0100

    Update miner.py

--- miner.py
+++ miner.py
@@ -1,36 +1,60 @@
-import threading
 import time
-from hashlib import sha256
-from base58 import b58encode_check
-from bitcoinrpc.authproxy import AuthServiceProxy
-from termcolor import colored
+import secrets
+import hashlib
+import base58
+import requests
 
-class Miner(threading.Thread):
-    def __init__(self):
-        threading.Thread.__init__(self)
+from colorama import init, Fore
 
-        self.daemon = True
-        self.finished = False
-        self.found = False
-        self.balance = 0
-        self.wallet = ""
-        self.priv = ""
+init()
 
-    def run(self):
-        while not self.finished and not self.found:
-            seed = str(time.time() * 1000).encode('utf-8')
-            priv = sha256(seed).hexdigest()
-            pub = b58encode_check(bytes.fromhex("80" + priv))
-            wallet = b58encode_check(bytes.fromhex("00" + pub))
-            try:
-                rpc_connection = AuthServiceProxy("http://127.0.0.1:8332", timeout=10)
-                balance = rpc_connection.getbalance(wallet.decode("utf-8"))
-                self.balance = balance
-                self.wallet = wallet.decode("utf-8")
-                self.priv = priv
-                self.found = True
-            except:
-                pass
+VALID_PAIR = Fore.GREEN + "Balance: {balance} | [✅] FOUND! {wallet} | Priv: {priv}"
+INVALID_PAIR = Fore.RED + "Balance: {balance} | [X] Trying! {wallet} | Priv: {priv}"
 
-    def stop(self):
-        self.finished = True
+def generate_private_key():
+    """Generate a new private key."""
+    while True:
+        private_key = hex(secrets.randbits(256))[2:]
+        if len(private_key) == 64:
+            return private_key
+
+def private_key_to_public_key(private_key):
+    """Convert a private key to a public key."""
+    private_key_bytes = bytes.fromhex(private_key)
+    public_key_bytes = hashlib.sha256(private_key_bytes).digest()
+    return public_key_bytes.hex()
+
+def public_key_to_wallet_address(public_key):
+    """Convert a public key to a wallet address."""
+    public_key_bytes = bytes.fromhex(public_key)
+    hash_bytes = hashlib.new('ripemd160', public_key_bytes).digest()
+    return base58.b58encode_check(b'\x00' + hash_bytes).decode()
+
+def get_wallet_balance(wallet_address):
+    """Get the balance of a wallet address."""
+    try:
+        response = requests.get(f"https://blockchain.info/q/addressbalance/{wallet_address}")
+        if response.status_code == 200:
+            balance = int(response.text)
+            return balance
+        else:
+            return None
+    except:
+        return None
+
+def mine_wallet():
+    """Mine a new wallet."""
+    while True:
+        private_key = generate_private_key()
+        public_key = private_key_to_public_key(private_key)
+        wallet_address = public_key_to_wallet_address(public_key)
+        balance = get_wallet_balance(wallet_address)
+
+        if balance is not None:
+            print(VALID_PAIR.format(balance=balance, wallet=wallet_address, priv=private_key))
+            break
+        else:
+            print(INVALID_PAIR.format(balance=balance, wallet=wallet_address, priv=private_key))
+
+if __name__ == "__main__":
+    mine_wallet()
